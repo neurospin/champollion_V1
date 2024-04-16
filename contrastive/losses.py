27314e33a2930fe5c29f9d4f31e09a3a32cef913
Julien Laval <laval.julien1@gmail.com>
2024-04-16 15:27:03 +0200

    drop last batch added in datamodule learning, to prevent from single subject batch, which is incompatible with BarlowTwins loss

--- contrastive/losses.py
+++ contrastive/losses.py
@@ -107,6 +107,8 @@ class BarlowTwinsLoss(nn.Module):
 
     def forward(self, z_a, z_b):
         # normalize repr. along the batch dimension
+        # beware: normalization is not robust to batch of size 1
+        # if it happens, it will return a nan loss
         z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
         z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
 
