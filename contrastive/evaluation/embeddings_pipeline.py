a11de213e4ce84490db7e16e00116ee5d9137861
Julien Laval <laval.julien1@gmail.com>
2024-06-20 09:56:15 +0200

    commit all

--- contrastive/evaluation/embeddings_pipeline.py
+++ contrastive/evaluation/embeddings_pipeline.py
@@ -202,12 +202,12 @@ def embeddings_pipeline(dir_path, datasets, labels, short_name=None, classifier_
             print(f"{sub_dir} is a file. Continue.")
 
 if __name__ == "__main__":
-    embeddings_pipeline("/volatile/jl277509/Runs/02_STS_babies/Program/Output/ORBITAL_12-layer_1-5mm_k11",
-        datasets=["julien/MICCAI_2024/evaluation/orbital_left_hcp_custom_1-5mm"],
-        labels=['Left_OFC'],
-        short_name='troiani', overwrite=True, embeddings=True, embeddings_only=False, use_best_model=False,
-        subsets=['full'], epochs=[None], split='custom', cv=3,
-        splits_basedir='/neurospin/dico/data/deep_folding/current/datasets/orbital_patterns/Troiani/train_val_split_',
+    embeddings_pipeline("/volatile/jl277509/Runs/02_STS_babies/Program/Output/test_PCS",
+        datasets=["julien/MICCAI_2024/evaluation/cingulate_right_ACCpatterns_custom"],
+        labels=['Right_PCS'],
+        short_name='ACC', overwrite=True, embeddings=True, embeddings_only=False, use_best_model=False,
+        subsets=['test'], epochs=[None], split='custom', cv=3,
+        splits_basedir='/neurospin/dico/data/deep_folding/current/datasets/ACCpatterns/ACCpatterns_subjects_train_split_',
         verbose=False)
     
 """Isomap Cingulate (hcp)
